1f077875a2a58457b27ca39db82566ad0bbcef3a
Levine-l <ucfnhl0@ucl.ac.uk>
2026-02-21 19:06:02 +0000

    add a intro to readme

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import MapDisplay from './components/MapDisplay'
 export default function App() {
   return (
     <div style={{ padding: 16 }}>
-      <h1 style={{ marginBottom: 12 }}>London Rail Map</h1>
+      <h1 style={{ marginBottom: 12 }}>London Rail Map & Residents Distance to Work Explore</h1>
       <MapDisplay />
     </div>
   )
